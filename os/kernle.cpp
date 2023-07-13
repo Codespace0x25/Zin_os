76e2ec860a26e61fde9361d61cfe0fefaf987da7
Zentech <77221503+minecraftteet@users.noreply.github.com>
2023-07-12 21:03:20 -0500

    Update kernle.cpp

--- os/kernle.cpp
+++ os/kernle.cpp
@@ -5,7 +5,8 @@ void printf(char* str)
 	for(int i=0; str[i] != '\0'; ++i)
 		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
-
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
 
 extern "C" void kernelMain(void* multiboot_structure,unsigned int magicnum)
 {
