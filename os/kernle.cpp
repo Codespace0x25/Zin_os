576e85b8a5c5cecaedbe63ac54bb46fe94376eb0
Zentech <77221503+minecraftteet@users.noreply.github.com>
2023-07-12 22:53:22 -0500

    Update kernle.cpp

--- os/kernle.cpp
+++ os/kernle.cpp
@@ -5,14 +5,6 @@
 }
 
 
-typedef void (*constructor)();
-extern "C" constructor start_ctors;
-extern "C" constructor end_ctors;
-extern "C" void callConstructors()
-{
-	for(constustructors* i = start_ctors; i != end_ctors; i++)
-		(*i)();
-}
 
 
 extern "C" void kernelMain(void* multiboot_structure,unsigned int magicnum)
