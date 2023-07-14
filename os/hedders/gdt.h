6ce06b1610af81637631cc6a77f8828dbcacf65d
Zentech <77221503+minecraftteet@users.noreply.github.com>
2023-07-14 02:58:22 -0500

    Add files via upload

---
+++ os/hedders/gdt.h
@@ -0,0 +1,41 @@
+#ifndef __GOT_N
+#define __GOT_N
+
+#include "type.h"
+
+  class GlobalDescriptorTable
+  {
+    
+    public:
+      class SegmentDescriptor
+      {
+        private:
+          uint16_t limit_lo;
+          uint16_t base_lo;
+          uint8_t base_hi;
+          uint8_t type;
+          uint8_t flags_limit_hi;
+          uint8_t base_vhi;
+        
+        public:
+          SegmentDescriptor(uint32_t base, uint32_t limit, uint8_t type);
+          uint32_t Base();
+          uint32_t Limit();
+      }__attribute__((packed));
+    
+    SegmentDescriptor nullSegmentselector;
+    SegmentDescriptor unusedSegmentselector;
+    SegmentDescriptor codeSegmentselector;
+    SegmentDescriptor dataSegmentselector;
+    
+    public:
+      GlobalDescriptorTable();
+      -GlobalDescriptorTable();
+      
+      uint16_t codeSegmentselector();
+      uint16_t dataSegmentselector(); 
+  };
+
+
+
+#endif
