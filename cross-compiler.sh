c9ddaa5caa5a66ba2e48dda5e8af0bf78af795d9
potatoJs <103060805+potato-Js@users.noreply.github.com>
2023-07-15 18:40:56 +0000

    made fixies to the cross compiler script

--- cross-compiler.sh
+++ cross-compiler.sh
@@ -8,7 +8,7 @@ export PATH="$PREFIX/bin:$PATH"
 
 # -- Preparing Dir --
 # if it fails , it maybe because /tmp was low on space, use another dir
-sudo mkdir -p /tmp/src
+mkdir -p /tmp/src
 cd /tmp/src
 
 # -- BINUTILS --
@@ -25,25 +25,25 @@ curl -O https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.gz #140mb
 tar xf gcc-12.2.0.tar.gz
 mkdir gcc-build
 cd gcc-build
-echo Configure: . . . . . . .
+echo "Configure: . . . . . . ."
 ../gcc-12.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-language=c,c++ --without-headers
-echo MAKE ALL-GCC:
+echo "MAKE ALL-GCC:"
 sudo make all-gcc
-echo MAKE ALL-TARGET-LIBGCC:
+echo "MAKE ALL-TARGET-LIBGCC:"
 sudo make all-target-libgcc
-echo MAKE INSTALL-GCC:
+echo "MAKE INSTALL-GCC:"
 sudo make install-gcc
-echo MAKE INSTALL-TARGET-LIBGCC:
+echo "MAKE INSTALL-TARGET-LIBGCC:"
 sudo make install-target-libgcc
-echo HERE U GO MAYBE:
-ls /usr/local/x86_64elfgcc/bin
+echo "HERE U GO MAYBE:"
+[ -d "/usr/local/x86_64elfgcc/bin" ] && ls /usr/local/x86_64elfgcc/bin  || { echo "failed to install"; && exit; }
 
 if [ -f "$HOME/.zshrc" ]; then
 	echo "ZSH shell found"
-	echo 'export PATH="$PATH:/usr/local/i386elfgcc/bin"' >>~/.zshrc
+	echo 'export PATH="$PATH:/usr/local/x86_64elfgcc/bin"' >>~/.zshrc
 elif [ -f "$HOME/.bashrc" ]; then
 	echo "BASHRC shell found"
-	echo 'export PATH="$PATH:/usr/local/i386elfgcc/bin"' >>~/.bashrc
+	echo 'export PATH="$PATH:/usr/local/x86_64elfgcc/bin"' >>~/.bashrc
 else
 	echo "[ ERR ] could not add to path" && exit
 fi
