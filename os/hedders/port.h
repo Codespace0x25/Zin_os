b31e6c8c2ae9937270de96fb6229878d74672307
Olivia <77221503+minecraftteet@users.noreply.github.com>
2023-07-17 14:07:58 +0000

    info

--- os/hedders/port.h
+++ os/hedders/port.h
@@ -1,52 +1,49 @@
 #ifndef __PORT_H
 #define __PORT_H
 
-
-
 #include "type.h"
 
+class Port
+{
+protected:
+    uint16_t portnum;
+    Port(uint16_t portnum);
+    ~Port();
+};
 
-    class Port
-    {
-     protected:
-       uint16_t portnum;
-       Port(uint16_t portnum);
-       ~Port();
-    };
+class Port8Bit : public Port
+{
+public:
+    Port8Bit(uint16_t portnum);
+    ~Port8Bit();
+    virtual void Write(uint8_t data) override;
+    virtual uint8_t Read() override;
+};
 
-    class Port8Bit : public Port
-    {
-     public:
-       Port8Bit(uint16_t portnum);
-       ~Port8Bit();
-       virtual void Wr￼ite(uint8_t data);
-       virtual uint8_t Read();
-    };
+class Port8BitSlow : public Port8Bit
+{
+public:
+    Port8BitSlow(uint16_t portnum);
+    ~Port8BitSlow();
+    virtual void Write(uint8_t data) override;
+};
 
-    class Port8BitSlow : public Port8Bit
-    {
-     public:
-       Port8BitSlow(uint16_t portnum);
-       ~Port8BitSlow();
-       virtual void Write(uint8_t data);
-    };
+class Port16Bit : public Port
+{
+public:
+    Port16Bit(uint16_t portnum);
+    ~Port16Bit();
+    virtual void Write(uint16_t data) override;
+    virtual uint16_t Read() override;
+};
 
-    class Port16Bit : public Port
-    {
-     public:
-       Port16Bit(uint16_t portnum);
-       ~Port16Bit();
-       virtual void Write(uint16_t data);
-       virtual uint16_t Read();
-    };
-    
-    class Port32Bit : public Port
-    {
-     public:
-       Port32Bit(uint16_t portnum);
-       ~Port32Bit();
-       virtual void Write(uint32_t data);
-       virtual uint32_t Read();
-    };
+class Port32Bit : public Port
+{
+public:
+    Port32Bit(uint16_t portnum);
+    ~Port32Bit();
+    virtual void Write(uint32_t data) override;
+    virtual uint32_t Read() override;
+};
 
 #endif
