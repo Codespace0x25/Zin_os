a083e49267cb726531d83b97f44194096983dbf1
Olivia <77221503+minecraftteet@users.noreply.github.com>
2023-07-20 21:41:44 +0000

    Heidi

--- os/hedders/interrupts.h
+++ os/hedders/interrupts.h
@@ -5,58 +5,51 @@
 #include "type.h"
 #include "gdt.h"
 
-  class InterruptMan
-  {
-    
-    
-    protected:
-      struct GateDescriptor
-      {
+class InterruptMan
+{
+protected:
+    struct GateDescriptor
+    {
         uint16_t handlerAddressLowBits;
         uint16_t gdt_codeSegmentSelector;
         uint8_t reserved;
         uint8_t access;
         uint16_t handlerAddressHighBits;
-        
-        
-      } __attribute__((packed));
-      
-      static GateDescriptor interruptDescriptorTable(256);
-      
-      static InterruptDescriptorTablePointer
-      {
-        unit16_t size;
+    } __attribute__((packed));
+
+    static GateDescriptor interruptDescriptorTable[256];
+
+public:
+    typedef struct
+    {
+        uint16_t size;
         uint32_t base;
-      } __attribute__((packed));
-      
-      
-      static void SetInterruptDescriptorTableEntry(
+    } __attribute__((packed)) InterruptDescriptorTablePointer;
+
+protected:
+    static void SetInterruptDescriptorTableEntry(
         uint8_t interruptNum,
         uint16_t gdt_codeSegmentorOffset,
         void (*handle)(),
         uint8_t DescriptorPrivilegeLevel,
-        uint_r DescriptorType
-      );
-      
-      port8BitSlow picMasterCommand;
-      port8BitSlow picMasterData;
-      port8BitSlow picSlaveCommand;
-      port8BitSlow picSlaveData;
-    
-    public:
-      
-      InterruptMan(GlobalDescriptorTable* gdt);
-      ~InterruptMan();
-      
-      void Activate();
-      
-      static uint32_t handleInterrupt(uint8_t interruptNum, uint32_t esp);
-      static void IgnoreInterruptRequest();
-      static void HamdInterruptRequest0x00();
-      static void HamdInterruptRequest0x01();
-  };
-  
-  
-  
+        uint8_t DescriptorType
+    );
 
-#endif
+    port8BitSlow picMasterCommand;
+    port8BitSlow picMasterData;
+    port8BitSlow picSlaveCommand;
+    port8BitSlow picSlaveData;
+
+public:
+    InterruptMan(GlobalDescriptorTable* gdt);
+    ~InterruptMan();
+
+    void Activate();
+
+    static uint32_t handleInterrupt(uint8_t interruptNum, uint32_t esp);
+    static void IgnoreInterruptRequest();
+    static void HamdInterruptRequest0x00();
+    static void HamdInterruptRequest0x01();
+};
+
+#endif // __INTERRUPTS_H
--- os/interrupts.cpp
+++ os/interrupts.cpp
@@ -1,76 +1,88 @@
 #include "./hedders/interrupts.h"
-#include "./hedders/outP.h"
 
+// Define the static member here
+InterruptMan::GateDescriptor InterruptMan::interruptDescriptorTable[256];
 
-InterruptMan::GateDescriptor InterruptMan::interrupDescriptorTable[256]
-
-static void InterruptMan::SetInterruptDescriptorTableEntry(
-  uint8_t interruptNum,
-  uint16_t gdt_codeSegmentorOffset,
-  void (*handle)(),
-  uint8_t DescriptorPrivilegeLevel,
-  uint_r DescriptorType
+void InterruptMan::SetInterruptDescriptorTableEntry(
+    uint8_t interruptNum,
+    uint16_t gdt_codeSegmentorOffset,
+    void (*handle)(),
+    uint8_t DescriptorPrivilegeLevel,
+    uint8_t DescriptorType
 )
 {
-  
-  const uint8_t IDT_DESC_PRESENT = 0x80;
-  
-  interrupDescriptorTable[interruptNum].handlerAddressLowBits = ((uint32_t)handler) & 0xFFFF;
-  interrupDescriptorTable[interruptNum].handlerAddressHighBits = (((uint32_t)handler) >> 16) & 0xFFFF;
-  interrupDescriptorTable[interruptNum].gdt_codeSegmentorOffset;
-  interrupDescriptorTable[interruptNum].access =uint8_t IDT_PRESENT | DescriptorType | (DescriptorPrivilegeLevel&3 << 5);
-  interrupDescriptorTable[interruptNum].reserved = 0;
-  
+    const uint8_t IDT_DESC_PRESENT = 0x80;
+
+    interruptDescriptorTable[interruptNum].handlerAddressLowBits = ((uint32_t)handle) & 0xFFFF;
+    interruptDescriptorTable[interruptNum].handlerAddressHighBits = (((uint32_t)handle) >> 16) & 0xFFFF;
+    interruptDescriptorTable[interruptNum].gdt_codeSegmentSelector = gdt_codeSegmentorOffset; // Fix the assignment
+    interruptDescriptorTable[interruptNum].access = IDT_DESC_PRESENT | DescriptorType | ((DescriptorPrivilegeLevel & 3) << 5);
+    interruptDescriptorTable[interruptNum].reserved = 0;
 }
 
 InterruptMan::InterruptMan(GlobalDescriptorTable* gdt)
-: picMasterCommand(0x20),
-  picMasterData(0x21),
-  picSlaveCommand(0xA0),
-  picSlaveData(0xA1)
+    : picMasterCommand(0x20),
+      picMasterData(0x21),
+      picSlaveCommand(0xA0),
+      picSlaveData(0xA1)
 {
-  uint16_t CodeSegment = gdt->CodeSegmentSelector();
-  const uint8_t IDE_INTERRUPT_GATE = 0xE;
-  
-  for(uint16_t i=0; i<256;i++)
-    SetInterrupDescriptorTableEntry(i, CodeSegment, &HamdInterruptRequest, 0, IDE_INTERRUPT_GATE);
-  
-  SetInterrupDescriptorTableEntry(0x20, CodeSegment, &HamdInterruptRequest0x00, 0, IDE_INTERRUPT_GATE);
-  SetInterrupDescriptorTableEntry(0x21, CodeSegment, &HamdInterruptRequest0x01, 0, IDE_INTERRUPT_GATE);
-  
-  picMasterCommand.Write(0x11);
-  picSlaveCommand.Write(0x11);
-  
-  picMasterData.Write(0x20);
-  picSlaveData.Write(0x28);
+    uint16_t CodeSegment = gdt->CodeSegmentSelector();
+    const uint8_t IDE_INTERRUPT_GATE = 0xE;
 
-  picMasterData.Write(0x04);
-  picSlaveData.Write(0x02);
-  
-  picMasterData.Write(0x01);
-  picSlaveData.Write(0x01);
-  
-  picMasterData.Write(0x00);
-  picSlaveData.Write(0x00);
-  
-  interruptDescriptorTablePointer idt;
-  idt.size = 256 * sizeof(GateDescriptor) -1;
-  idt.base = (uint_t)interrupDescriptorTable;
-  asm volatile("lidt %0": :"m"'(idt));
-  
+    for (uint16_t i = 0; i < 256; i++)
+        SetInterruptDescriptorTableEntry(i, CodeSegment, &IgnoreInterruptRequest, 0, IDE_INTERRUPT_GATE);
+
+    SetInterruptDescriptorTableEntry(0x20, CodeSegment, &HamdInterruptRequest0x00, 0, IDE_INTERRUPT_GATE);
+    SetInterruptDescriptorTableEntry(0x21, CodeSegment, &HamdInterruptRequest0x01, 0, IDE_INTERRUPT_GATE);
+
+    picMasterCommand.Write(0x11);
+    picSlaveCommand.Write(0x11);
+
+    picMasterData.Write(0x20);
+    picSlaveData.Write(0x28);
+
+    picMasterData.Write(0x04);
+    picSlaveData.Write(0x02);
+
+    picMasterData.Write(0x01);
+    picSlaveData.Write(0x01);
+
+    picMasterData.Write(0x00);
+    picSlaveData.Write(0x00);
+
+    InterruptDescriptorTablePointer idt;
+    idt.size = 256 * sizeof(GateDescriptor) - 1;
+    idt.base = (uint32_t)interruptDescriptorTable; // Fix the variable name
+    asm volatile("lidt %0": :"m"(idt));
 }
+
 InterruptMan::~InterruptMan()
 {
 }
 
 void InterruptMan::Activate()
 {
-  asm("sti")
+    asm("sti"); // Enable interrupts
 }
 
 uint32_t InterruptMan::handleInterrupt(uint8_t interruptNum, uint32_t esp)
 {
-  putText("Interrupt\n")
-  
-  return est;
+    putText("Interrupt\n");
+    return esp; // Return the stack pointer as intended
+}
+
+// Implement the rest of your interrupt handling functions here
+void InterruptMan::IgnoreInterruptRequest()
+{
+    // Do nothing or print a message indicating an ignored interrupt
+}
+
+void InterruptMan::HamdInterruptRequest0x00()
+{
+    // Handle interrupt 0x00
+}
+
+void InterruptMan::HamdInterruptRequest0x01()
+{
+    // Handle interrupt 0x01
 }
